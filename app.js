7aeccc6f04c8a2e1590ab2639236184280db8a9b
Moto Chowdhury <motochowdhury@gmail.com>
2022-12-20 18:31:55 +0600

    Setup and configure database and Parser

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 dotenv.config();
 
 // Database Connecton
+mongoose.set("strictQuery", true);
 mongoose
   .connect(process.env.MONGO_CONNECTION, {
     useNewUrlParser: true,
@@ -18,3 +21,21 @@ mongoose
 // Request Parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Set View Engine
+app.set("view engine", "ejs");
+
+// Set Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Parse Cookies
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+// Routing Setup
+
+// Error Handling
+
+// Listener
+app.listen(process.env.PORT, () => {
+  console.log("server is running at", process.env.PORT);
+});
