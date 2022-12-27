bc31cb0a69c9c3e4e2f7329f9d91a253668bebed
motochowdhury <motochowdhury@gmail.com>
2022-12-27 11:33:13 +0600

    Schema Creation Done

---
+++ models/People.js
@@ -0,0 +1,46 @@
+// external Imports
+const mongoose = require("mongoose");
+
+// Schema
+const PeopleSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      lowercase: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "user"],
+      default: "user",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// Model
+
+const People = mongoose.model("people", PeopleSchema);
+
+// Module exports
+
+module.exports = People;
